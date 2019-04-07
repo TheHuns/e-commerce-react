4d159f4ef5d1b73944a1e416b3ba5a25cb8a93e7
Dehn Hunsworth <dhunsworth@yahoo.com>
2019-04-06 23:53:53 -0600

    base data for products

---
+++ src/data.js
@@ -0,0 +1,123 @@
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Jackson Traverse",
+    img: "img/jackson-traverse.jpg",
+    price: 1199,
+    company: "JACKSON",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore corrupti hic veritatis, omnis saepe distinctio dicta molestiae ratione, molestias necessitatibus amet fugiat libero maxime magnam fuga ipsa? Illo, doloribus? Ullam.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Jackson Zen",
+    img: "img/jackson-zen.jpg",
+    price: 1149,
+    company: "JACKSON",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore corrupti hic veritatis, omnis saepe distinctio dicta molestiae ratione, molestias necessitatibus amet fugiat libero maxime magnam fuga ipsa? Illo, doloribus? Ullam.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Jackson Antix",
+    img: "img/jackson-antix.jpg",
+    price: 1099,
+    company: "JACKSON",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore corrupti hic veritatis, omnis saepe distinctio dicta molestiae ratione, molestias necessitatibus amet fugiat libero maxime magnam fuga ipsa? Illo, doloribus? Ullam.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Dagger Mamba",
+    img: "img/dagger-mamba.png",
+    price: 999,
+    company: "DAGGER",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore corrupti hic veritatis, omnis saepe distinctio dicta molestiae ratione, molestias necessitatibus amet fugiat libero maxime magnam fuga ipsa? Illo, doloribus? Ullam.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Dagger Nomad",
+    img: "img/dagger-nomad.png",
+    price: 1099,
+    company: "DAGGER",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore corrupti hic veritatis, omnis saepe distinctio dicta molestiae ratione, molestias necessitatibus amet fugiat libero maxime magnam fuga ipsa? Illo, doloribus? Ullam.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Dagger Axiom",
+    img: "img/dagger-axiom.png",
+    price: 899,
+    company: "DAGGER",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore corrupti hic veritatis, omnis saepe distinctio dicta molestiae ratione, molestias necessitatibus amet fugiat libero maxime magnam fuga ipsa? Illo, doloribus? Ullam.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Liquid Logic Remix Xp",
+    img: "img/liquidlogic-remixxp.jpg",
+    price: 1299,
+    company: "LIQUID LOGIC",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore corrupti hic veritatis, omnis saepe distinctio dicta molestiae ratione, molestias necessitatibus amet fugiat libero maxime magnam fuga ipsa? Illo, doloribus? Ullam.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Liquid Logic Remix",
+    img: "img/liquidlogic-remix.jpg",
+    price: 1099,
+    company: "LIQUID LOGIC",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore corrupti hic veritatis, omnis saepe distinctio dicta molestiae ratione, molestias necessitatibus amet fugiat libero maxime magnam fuga ipsa? Illo, doloribus? Ullam.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 9,
+    title: "Liquid Logic Delta V",
+    img: "img/liquidlogic-deltav.jpg",
+    price: 1199,
+    company: "LIQUID LOGIC",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore corrupti hic veritatis, omnis saepe distinctio dicta molestiae ratione, molestias necessitatibus amet fugiat libero maxime magnam fuga ipsa? Illo, doloribus? Ullam.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Jackson Traverse",
+  img: "img/jackson-traverse.jpg",
+  price: 1199,
+  company: "JACKSON",
+  info:
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore corrupti hic veritatis, omnis saepe distinctio dicta molestiae ratione, molestias necessitatibus amet fugiat libero maxime magnam fuga ipsa? Illo, doloribus? Ullam.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
