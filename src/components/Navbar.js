ce803fc3a897bf205e0219c368eaa1c0a3820efe
Dehn Hunsworth <dhunsworth@yahoo.com>
2019-04-06 23:54:59 -0600

    product card client basics

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,47 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { ButtonContainer } from "./Button";
 
 class Navbar extends Component {
-   state = {};
-   render() {
-      return (
-         <div>
-            <h3>Navbar</h3>
-         </div>
-      );
-   }
+  state = {};
+  render() {
+    return (
+      <NavWrapper className="navbar navbar-expand-sm  navbar-dark px-sm-5">
+        <Link to="/">
+          <i className="fas fa-tint fa-2x" />
+        </Link>
+        <ul className="navbar-nav align-items-center ">
+          <li className="nav-item ">
+            <Link to="/" className="nav-link ml-2">
+              products
+            </Link>
+          </li>
+        </ul>
+        <Link to="/cart" className="ml-auto">
+          <ButtonContainer>
+            <span className="mr-2">
+              <i className="fas fa-cart-plus" />
+            </span>
+            My Cart
+          </ButtonContainer>
+        </Link>
+      </NavWrapper>
+    );
+  }
 }
+
+const NavWrapper = styled.nav`
+  background: var(--mainBlue);
+  .nav-link {
+    color: var(--mainWhite) !important;
+    font-size: 1.3rem;
+    text-transform: capitalize !important;
+    letter-spacing: -0.5px;
+  }
+  i {
+    transition: all 0.5s linear;
+  }
+`;
 
 export default Navbar;
